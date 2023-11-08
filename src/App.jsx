67619e99941202285934a129c0e77945abfa3728
Hasan <thehasandev@gmail.com>
2023-11-08 20:34:13 +0600

    goggle-fonts-add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import Button from '@mui/material/Button';
+
 function App() {
   return (
-    <div>
-      <Button variant="contained" disableElevation>
-        Disable elevation
-      </Button>
-    </div>
+    <div>App</div>
   )
 }
 
