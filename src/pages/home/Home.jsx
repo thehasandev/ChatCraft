6dcf0b16ad265a06aebfa8756e4c8ce959245f0b
Hasan <thehasandev@gmail.com>
2023-11-21 11:02:11 +0600

    logout-web

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import { getAuth, signOut } from "firebase/auth";
 
 function Home() {
+  const auth = getAuth();
   let navigate = useNavigate()
   let handleLogout =()=>{
-    navigate("/")
+    signOut(auth).then(() => {
+      navigate("/")
+    }).catch((error) => {
+    });
   }
   return (
     <Button onClick={handleLogout} variant="contained">Logout</Button>
