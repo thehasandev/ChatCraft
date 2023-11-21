0ccb4ff829758eab43f7275bea5c45f572fb5a69
Hasan <thehasandev@gmail.com>
2023-11-21 10:59:59 +0600

    navigate-logout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  let navigate = useNavigate()
+  let handleLogout =()=>{
+    navigate("/")
+  }
   return (
-    <Button variant="contained">Logout</Button>
+    <Button onClick={handleLogout} variant="contained">Logout</Button>
   )
 }
 
