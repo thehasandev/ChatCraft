56631b1263952392ce88aa9b478a35d0d6bcf06a
Hasan <thehasandev@gmail.com>
2023-11-22 19:12:55 +0600

    icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import profileImg from "../assets/profile.png"
+import { FaHome } from "react-icons/fa";
+import { AiFillMessage } from "react-icons/ai";
 function Sidebar() {
 
   return (
     <div className='sidevar'>
-       
+      <div>
+        <img src={profileImg} alt="profile" />
+      </div>
+      <FaHome className='icons'/> 
+      <FaHome className='icons'/> 
+      <FaHome className='icons'/> 
+  
+
+   
     </div>
   )
 }
