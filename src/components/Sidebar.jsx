f3035dfef9203475905615c039b95f6397ed672d
Hasan <thehasandev@gmail.com>
2023-11-22 20:14:49 +0600

    sidevar-log-out

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,24 @@ import { FaHome } from "react-icons/fa";
 import { AiFillMessage } from "react-icons/ai";
 import { IoMdNotifications } from "react-icons/io";
 import { CiSettings } from "react-icons/ci";
+import { IoIosLogOut } from "react-icons/io";
+import { getAuth, signOut } from "firebase/auth";
+
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 function Sidebar() {
+  const auth = getAuth();
+  let userData =useSelector((state)=>state.loguser.value)
+  let dispatch = useDispatch()   
+  let navigate = useNavigate()
+
+  let handleLogout =()=>{
+    signOut(auth).then(() => {
+      localStorage.removeItem("user")
+      navigate("/")
+    }).catch((error) => {
+    });
+  }
 
   return (
     <div className='sidevar'>
@@ -12,8 +29,7 @@ function Sidebar() {
         <img src={profileImg} alt="profile" />
       </div>
      
-     
-
+      <div style={{marginBottom:"50px"}}>
         <div className='icons-part'>
             <div className=''>
               <FaHome className='icons'/> 
@@ -37,8 +53,15 @@ function Sidebar() {
               <CiSettings className='icons'/> 
             </div>
         </div>
+      </div>
+
+      <div className='icons-part'>
+          <IoIosLogOut onClick={handleLogout} className='icons'/> 
+       </div>
+
 
     </div>
+    
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,36 +2,25 @@ import React, { useEffect } from 'react'
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import { getAuth, signOut } from "firebase/auth";
-import { useDispatch, useSelector } from 'react-redux';
 import { activeuser } from '../../slices/firebaseUser';
+import { useDispatch, useSelector } from 'react-redux';
 
+// useEffect(()=>{
+//  if(userData == null){
+//   navigate("/")
+//  }
+// },[])
 
 function Home() {
-  let userData =useSelector((state)=>state.loguser.value)
-  
-  let dispatch = useDispatch()   
-  const auth = getAuth();
-  let navigate = useNavigate()
-  
-  let handleLogout =()=>{
-    signOut(auth).then(() => {
-      localStorage.removeItem("user")
-      dispatch(activeuser(null))
-      navigate("/")
-    }).catch((error) => {
-    });
-  }
 
-  useEffect(()=>{
-   if(userData == null){
-    navigate("/")
-   }
-  },[])
+  
+
+
   
 
   return (
     <>
-     <Button onClick={handleLogout} variant="contained">Logout</Button>
+     <Button  variant="contained">Logout</Button>
     </>
     
    )
