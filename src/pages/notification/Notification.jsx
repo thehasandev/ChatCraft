e9126af1c81952db4b98bd8b4cab3992b68a4001
Hasan <thehasandev@gmail.com>
2023-11-23 12:30:26 +0600

    navigate-part-four

--- src/pages/notification/Notification.jsx
+++ src/pages/notification/Notification.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function Notification() {
+  let userData =useSelector((state)=>state.loguser.value)
+  let navigate =useNavigate()
+
+  useEffect(()=>{
+    if(userData == null){
+       navigate("/")
+    }
+   },[])
+
   return (
     <div>Notification</div>
   )
