a1a2eb76dff333379284068ccdf7e7de68b9d374
Hasan <thehasandev@gmail.com>
2023-11-25 20:14:19 +0600

    search-filter-users

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -4,9 +4,11 @@ import { BiDotsVerticalRounded } from "react-icons/bi";
 import { getDatabase, ref, onValue } from "firebase/database";
 function UserList() {
   const db = getDatabase();
-  let [userList,setUserList] =useState([])
-
+  let [userList,setUserList] = useState([])
+  let [input,setInput] =useState("")
+  let [searchUserList,setSearchUserList] =useState([])
   
+
   useEffect(()=>{
     const starCountRef = ref(db, 'users');
     onValue(starCountRef, (snapshot) => {
@@ -19,32 +21,61 @@ function UserList() {
   },[])
 
 
+  let handleUserChange =(e)=>{
+    setInput(e.target.value)
+   let searchUser = userList.filter((item=> item.userName.toLowerCase().includes(e.target.value.toLowerCase())))
+   setSearchUserList(searchUser);
+  }
   return (
     
     <div className='list'>
-      
+
       <div className="list-box">
         <h2>User List </h2>
-        <input type="text" placeholder='Search a Users'/>
+        <input onChange={handleUserChange} type="text" placeholder='Search a Users'/>
         <BiDotsVerticalRounded />
       </div>
-    
+
     {
+      input.length < 1 ?
       userList.map((item,index)=>( 
-      <div key={index} className='list-item'>
-        <div>
-          <img src={item.userImgUrl} alt="g1" />
+        <div key={index} className='list-item'>
+          <div>
+            <img src={item.userImgUrl} alt="g1" />
+          </div>
+          <div>
+              <h3>{item.userName}</h3>
+              <p>Today, 2:31pm</p>
+          </div>
+          <div>
+              <button>+</button>
+          </div>
         </div>
-        <div>
-            <h3>{item.userName}</h3>
-            <p>Today, 2:31pm</p>
-        </div>
-        <div>
-            <button>+</button>
-        </div>
-      </div>
-      ))
+        ))  
+        :
+        searchUserList.length>0 ?
+        searchUserList.map((item,index)=>( 
+          <div key={index} className='list-item'>
+            <div>
+              <img src={item.userImgUrl} alt="g1" />
+            </div>
+            <div>
+                <h3>{item.userName}</h3>
+                <p>Today, 2:31pm</p>
+            </div>
+            <div>
+                <button>+</button>
+            </div>
+          </div>
+          ))  
+        :
+        <h1>Not fount</h1>
+        
+
     }
+   
+ 
+  
       
 
       
