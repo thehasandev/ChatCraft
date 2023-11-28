4ec7bb76ddedbd3b3da89f5af1a62ccd0cbbe3d0
Hasan <thehasandev@gmail.com>
2023-11-28 13:00:07 +0600

    search-four

--- src/components/GroupList.jsx
+++ src/components/GroupList.jsx
@@ -1,69 +1,78 @@
 import React, { useEffect } from 'react'
 import gOne from "../assets/g2.png"
 import { BiDotsVerticalRounded } from "react-icons/bi";
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { IoSearchSharp } from "react-icons/io5";
+
 function GroupList() {
 
   return (
     <div className='list'>
+      <div className='input'>
+        <input  type="text" placeholder='Search a Users'/>
+
+        <div className='icon'>
+          <IoSearchSharp/>
+        </div>
+      </div> 
+
       <h2>Groups List <BiDotsVerticalRounded /></h2>
-    <div className='scroll'>
-      <div className='list-item'>
-        <div>
-          <img src={gOne} alt="g1" />
-        </div>
-        <div>
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div>
-            <button>Join</button>
-        </div>
-      </div>
-      
-      <div className='list-item'>
-        <div>
-          <img src={gOne} alt="g1" />
-        </div>
-        <div>
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div>
-            <button>Join</button>
-        </div>
-      </div>
-      
-      <div className='list-item'>
-        <div>
-          <img src={gOne} alt="g1" />
-        </div>
-        <div>
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div>
-            <button>Join</button>
-        </div>
-      </div>
 
-      <div className='list-item'>
-        <div>
-          <img src={gOne} alt="g1" />
+      <div className='scroll'>
+        <div className='list-item'>
+          <div>
+            <img src={gOne} alt="g1" />
+          </div>
+          <div>
+              <h3>Friends Reunion</h3>
+              <p>Hi Guys, Wassup!</p>
+          </div>
+          <div>
+              <button>Join</button>
+          </div>
+        </div>
+        
+        <div className='list-item'>
+          <div>
+            <img src={gOne} alt="g1" />
+          </div>
+          <div>
+              <h3>Friends Reunion</h3>
+              <p>Hi Guys, Wassup!</p>
+          </div>
+          <div>
+              <button>Join</button>
+          </div>
+        </div>
+        
+        <div className='list-item'>
+          <div>
+            <img src={gOne} alt="g1" />
+          </div>
+          <div>
+              <h3>Friends Reunion</h3>
+              <p>Hi Guys, Wassup!</p>
+          </div>
+          <div>
+              <button>Join</button>
+          </div>
         </div>
 
-        <div>
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
+        <div className='list-item'>
+          <div>
+            <img src={gOne} alt="g1" />
+          </div>
+
+          <div>
+              <h3>Friends Reunion</h3>
+              <p>Hi Guys, Wassup!</p>
+          </div>
+
+          <div>
+              <button>Join</button>
+          </div>
         </div>
 
-        <div>
-            <button>Join</button>
-        </div>
       </div>
-
-    </div>
 
 
     </div>
